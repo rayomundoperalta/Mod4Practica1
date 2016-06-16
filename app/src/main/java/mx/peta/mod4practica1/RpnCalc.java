5eece2d46c25dd0c23a457dbdf6db8bf842cd798
Raymundo Peralta <movile@peta.mx>
2016-06-16 00:40:34 -0500

    Primer Comit, ya esta definido el primer XML y el bloqueo de pantalla para que no gire

---
+++ app/src/main/java/mx/peta/mod4practica1/RpnCalc.java
@@ -0,0 +1,13 @@
+package mx.peta.mod4practica1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RpnCalc extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rpn_calc);
+    }
+}
